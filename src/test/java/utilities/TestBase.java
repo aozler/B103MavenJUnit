8b9c42118f40e8475dd39f39d8b859e4e11ecb5e
Arthur Ozler <ozlerart@gmail.com>
2023-01-03 19:27:18 -0800

    01/03/23

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,45 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+public abstract class TestBase {
+
+    //    driver objesini olustur. Driver ya public yada protected olmali. Sebepi child classlarda gorulebilir olmasi
+    protected static WebDriver driver;
+
+    //    setUp
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    //    tearDown
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+//        - MULTIPLE WINDOW -
+//    1 parametre alir : Gecis Yapmak Istedigim sayfanin Title
+//    ORNEK:
+//    driver.get("https://the-internet.herokuapp.com/windows");
+//    switchToWindow("New Window");
+//    switchToWindow("The Internet")
+    public static void switchToWindow(String targetTitle){
+        String origin = driver.getWindowHandle();
+        for (String handle : driver.getWindowHandles()){
+            driver.switchTo().window(handle);
+            if (driver.getTitle().equals(targetTitle)){
+                return; //CIK. break;
+            }
+        }
+        driver.switchTo().window(origin);
+    }
+}
