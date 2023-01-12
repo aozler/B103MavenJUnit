c9b072c8daae66d74109ea92dced9d4c741833b8
Arthur Ozler <ozlerart@gmail.com>
2023-01-12 09:40:25 -0800

    01/11/23

--- src/test/java/day05_JUnit/C04_BeforeClassAfterClass.java
+++ src/test/java/day05_JUnit/C04_BeforeClassAfterClass.java
@@ -1,10 +1,6 @@
 package day05_JUnit;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
 
 public class C04_BeforeClassAfterClass {
 
